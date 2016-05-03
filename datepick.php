935ec43df1b3a16725a296292cd488ff09386067
TendeRz <tendziks@gmail.com>
2016-05-03 15:53:56 +0300

    datetimepicker dynamic adding

--- datepick.php
+++ datepick.php
@@ -1,50 +1,70 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
-  <title>Date Picker</title>
-  
-  
-  <link rel="stylesheet" type="text/css" href="/root/PS/css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="/root/PS/css/bootstrap-datetimepicker.css" />
-  
+	<title>Date Picker</title>
 
-  <script src="/root/PS/js/jquery-2.2.0.js"> </script>
-  <script src="/root/PS/js/moment-with-locales.js"></script>
-  <script src="/root/PS/js/bootstrap.js"></script>
-  <script src="/root/PS/js/bootstrap-datetimepicker.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="/root/PS/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/root/PS/css/bootstrap-datetimepicker.css" />
+
+
+	<script src="/root/PS/js/jquery-2.2.0.js"> </script>
+	<script src="/root/PS/js/moment-with-locales.js"></script>
+	<script src="/root/PS/js/bootstrap.js"></script>
+	<script src="/root/PS/js/bootstrap-datetimepicker.js"></script>
 </head>
 <body>
 
 
 
-<div class="container">
-    <div class="row">
-        <div class='col-sm-6'>
-            <input type='text' class="form-control" id='datetimepicker4' />
-        </div>
-        <script type="text/javascript">
-            $(function () {
-                $('#datetimepicker4').datetimepicker({
-                  locale: 'en',
-                  format: "DD/MM/YYYY HH:mm",
-                  sideBySide: true
-                });
-            });
-        </script>
-    </div>
-</div>
+	<div class="container">
+		<div class="addtime">
+			<div class="row">
+				<div class='col-sm-12 timeset'>
+					
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+
+
+
+
+	<div class="row">
+		<button id="addTime" class="btn" style="margin: 20px 0 20px 20px">Select</button>
+	</div>
+
+	<script>
+		$(function () {
+			$('#addTime').on('click',function () {
+				$('.col-sm-12').append('<input type="text" class="form-control" />');
+				$('.timeset input').each(function () {
+
+					$(this).datetimepicker({
+						locale: 'en',
+						format: "HH:mm",
+						sideBySide: true
+					});
+				});
+			})
+		})
+	</script>
+
+	<script type="text/javascript">
+		
+	</script>
 
 
 
 
 
-
-</div>
 
 </body>
 </html>
